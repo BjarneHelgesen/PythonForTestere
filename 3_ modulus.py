25dbf3af07f1659f97ef8fda162e85d93bc861ea
Bjarne Helgesen <bjarne@helgesen.biz>
2025-05-06 17:25:27 +0200

    Added modulus example

---
+++ 3_ modulus.py
@@ -0,0 +1,10 @@
+
+example_list = range(20)
+print(len(example_list))
+for i in example_list:
+    if i % 2 == 0:
+        print(i, "er partall")
+    else:
+        print(i, "er oddetall")
+        
+    
