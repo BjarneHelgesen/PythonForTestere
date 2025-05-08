9c842f8b24c566f1c23bceffa72b8f734db77b22
Bjarne Helgesen <bjarne@helgesen.biz>
2025-05-08 17:24:08 +0200

    String functions

---
+++ 4_strenger.py
@@ -0,0 +1,14 @@
+
+print("Python" + " " + "kurs") # Nyttig til juletre
+eksempel = "Testing testing "
+print(eksempel[0])
+eksempel[0] = "t"
+print(eksempel[1:5])
+print(eksempel.strip())
+print("*" * 10) # Nyttig til juletre
+for char in eksempel:
+    print(char)
+print("Hello\nWorld")
+test_string = "Hallo\n" + \
+              "Verden" # Nyttig til juletre
+print(len("asdf"))
